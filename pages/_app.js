49c9ca7e2632850ad76ed2dc627c60693d77e619
pham bao <baopt>
2022-03-31 12:43:58 +0700

    .

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/tailwind.css";
 import "../styles/slick.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>VPN Best Landingpage </title>
+        <title>Cheer For Ukraine</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
